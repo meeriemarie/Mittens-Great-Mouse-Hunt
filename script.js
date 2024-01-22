54d2cfd7489ecf8dbc878776bff91a692957dd48
Marie Rohler <cc231054@fhstp.ac.at>
2024-01-22 09:19:41 +0100

    improved gameLoop and added map array

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 import {CatObject} from "./catObject.js"
-import {canvas,ctx} from "./canvas.js";
 import {MouseObject} from "./mouseObject.js";
+import {DogObject} from "./dogObject.js";
 import {Obstacle} from "./obstacle.js";
-// import {startGameClock, setNewTick, setDefaultFrameRate, interpolateValueToFPS} from "./FPSHelper.js";
-import {scoreBoard} from "./scoreBoard.js";
-import{handleCollisions} from "./Collisions.js";
+import {collisionDetection, updateGameObjects, renderGameObjects} from "./logicLayer.js"
 
 let mitten = new CatObject(16,16,0,208,0,0,0,0);
+let mice = [];
+let platforms = [];
+let doggos = [];
 
-let mice = [
-    new MouseObject(8,8,60,216,0,0,0,0,),
-    new MouseObject(8,8,166,184,0,0,0,0)
-    ];
+const map = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,3,0,0,0,0,0,0,0,0,0,0,0,2,2,2,0,0,0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,2,2,0,0,0,0,0,0,0,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0,0,2,2,0,0,0,0,0,3,0,0,0,0],
+    [0,0,0,0,0,0,2,0,0,0,2,3,0,0,0,0,0,0,2,2,0,0,0,0,0,0,0,0,0,0,2,2,0,0,0,0],
+    [0,0,0,0,2,2,2,0,1,0,2,2,2,0,0,1,0,2,2,2,0,0,1,0,0,0,0,1,0,2,2,2,2,0,0,0]
+]
 
-let platforms = [
-    new Obstacle(32,32,150,192,0,0,0,0),
-    new Obstacle(32,32,182,192,0,0,0,0),
-    //new Obstacle(32,32,182,160,0,0,0,0),
-    //new Obstacle(32,32,50,124,0,0,0,0),
-    //new Obstacle(32,32,38,124,0,0,0,0)
-];
+map.forEach((row, i) => {
+    row.forEach((number, j) => {
+        switch (number) {
+            case 1:
+                doggos.push(new DogObject(16,16,32*j + 8,32*i + 16,0,0,0,0))
+                break;
+            case 2:
+                platforms.push(new Obstacle(32,32, 32*j, 32*i,0,0,0,0))
+                break;
+            case 3:
+                mice.push(new MouseObject(8,8,32 * j + 12,32 * i + 24,0,0,0,0))
+                break;
+        }
 
-function updateGameObjects () {
-    mitten.updatePosition();
-}
+    })
+})
 
-function renderGameObjects () {
-    ctx.clearRect(0,0,canvas.width, canvas.height);
-    mitten.draw();
-    mice.forEach(mouse => {
-        mouse.draw();
-    });
-    platforms.forEach(platform => {
-        platform.draw();
-    });
-    scoreBoard.drawScore();
-}
 
 function gameLoop(){
-    handleCollisions();
+    collisionDetection();
     updateGameObjects();
     renderGameObjects();
     requestAnimationFrame(gameLoop);
@@ -83,4 +83,4 @@ addEventListener("keydown", keyEventDown);
 addEventListener("keyup", keyEventUp);
 requestAnimationFrame(gameLoop);
 
-export {platforms, mitten, mice}
+export {platforms, mitten, mice, doggos}
