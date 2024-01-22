75f29efe2382ebfe53e5704728638a509d6c9735
Marie Rohler <cc231054@fhstp.ac.at>
2024-01-22 09:14:40 +0100

    improved Collision

--- Collisions.js
+++ Collisions.js
@@ -1,45 +1,5 @@
 import {mitten, platforms, mice} from "./script.js";
 import {scoreBoard} from "./scoreBoard.js";
-// import {interpolateValueToFPS} from "./FPSHelper.js";
-
-
-/*
-function checkCollision(mitten, platforms) {
-    if(mitten.position.x < platforms.position.x + platforms.dimensions.width &&
-        mitten.position.x + mitten.dimensions.width > platforms.position.x)
-        if (mitten.position.y < platforms.position.y + platforms.dimensions.height &&
-            mitten.position.y + mitten.dimensions.height > platforms.position.y){
-            return true;
-        }
-}
-
-// Check collision between player and platforms
-function handleCollisions() {
-    platforms.forEach(platform => {
-        if (checkCollision(mitten, platform)) {
-            if (mitten.velocity.y > 0 && mitten.position.y + mitten.dimensions.height - mitten.velocity.y <= platform.position.y) {
-                mitten.velocity.y = 0;
-                mitten.position.y = platform.position.y - mitten.dimensions.height - mitten.velocity.y;
-                // Top collision
-                if (mitten.position.x > platform.position.x
-                    && mitten.position.x + mitten.dimensions.width < platform.position.x + platform.dimensions.width) {
-                    mitten.midair = false;
-                } else {
-                    mitten.midair = true;
-                }
-            } else if (mitten.velocity.y < 0 && mitten.position.y - mitten.velocity.y >= platform.position.y + platform.dimensions.height) {
-                mitten.velocity.y = 0;
-                mitten.position.y = platform.position.y + platform.dimensions.height + mitten.velocity.y;
-                // Bottom collision
-            } else if (mitten.velocity.x > 0 && mitten.position.x + mitten.velocity.x >= platform.position.x + mitten.velocity.x) {
-                mitten.velocity.x = 0;
-            } else {
-                mitten.position.x = platform.position.x - mitten.dimensions.width - mitten.velocity.x;
-            }
-        }
-    });
-}
- */
 
 function handleCollisions () {
     platforms.forEach(platform => {
@@ -50,7 +10,8 @@ function handleCollisions () {
             mitten.position.y + mitten.dimensions.height > platform.position.y
         ) {
             // Top collision
-            if (mitten.velocity.y > 0 && mitten.position.y + mitten.dimensions.height - mitten.velocity.y <= platform.position.y) {
+            if (mitten.velocity.y > 0 && mitten.position.y + mitten.dimensions.height - mitten.velocity.y <= platform.position.y
+                && mitten.position.x > platform.position.x && mitten.position.x + mitten.dimensions.width < platform.position.x + platform.dimensions.width) {
                 mitten.velocity.y = 0;
                 mitten.position.y = platform.position.y - mitten.dimensions.height;
                 mitten.midair = false;
@@ -77,12 +38,25 @@ function handleCollisions () {
                 mitten.position.x = platform.position.x + platform.dimensions.width;
             }
         }
+
     });
 }
 
 
+function checkIfMidAir(mittenX, platformX, platformWidth) {
+    return (mittenX < platformX || mittenX > (platformX + platformWidth));
+}
 
+function collectMice () {
+    mice.forEach((mouse, i) => {
+        if (mitten.position.x < mouse.position.x + mouse.dimensions.width &&
+            mitten.position.x + mitten.dimensions.width > mouse.position.x &&
+            mitten.position.y < mouse.position.y + mouse.dimensions.height &&
+            mitten.position.y + mitten.dimensions.height > mouse.position.y) {
+            mice.splice(i, 1);
+            scoreBoard.score ++;
+        }
+    })
+}
 
-
-
-export {handleCollisions}
+export {handleCollisions, collectMice}
