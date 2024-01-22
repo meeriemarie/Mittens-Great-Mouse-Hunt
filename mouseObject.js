5c4fa81fa51c901e9612bc043f3d2256b9758f3e
Marie Rohler <cc231054@fhstp.ac.at>
2024-01-22 09:17:20 +0100

    no changes

--- mouseObject.js
+++ mouseObject.js
@@ -2,7 +2,6 @@ import {gameObject} from "./gameObject.js";
 import {ctx} from "./canvas.js";
 
 class MouseObject extends gameObject {
-
     draw = () => {
         ctx.save();
         ctx.fillStyle = "#BA3B46";
