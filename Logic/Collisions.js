b0a27304c52074c7eed7f1a62a2be74deee701fe
Jakob Bal <baljakob9@gmail.com>
2024-01-25 17:03:00 +0100

    Extended collsion detection to feature checks for falling down

--- Logic/Collisions.js
+++ Logic/Collisions.js
@@ -1,80 +1,134 @@
-import {mitten, platforms, mice, doggos, health} from "./script.js";
-import {scoreBoard} from "../GameObjects/scoreBoard.js";
+import { mitten, platforms, mice, doggos, health } from './script.js';
+import { scoreBoard } from '../GameObjects/scoreBoard.js';
 
-function handleCollisions () {
-    platforms.forEach(platform => {
-        if (
-            mitten.position.x < platform.position.x + platform.dimensions.width &&
-            mitten.position.x + mitten.dimensions.width > platform.position.x &&
-            mitten.position.y < platform.position.y + platform.dimensions.height &&
-            mitten.position.y + mitten.dimensions.height > platform.position.y
-        ) {
-            // Top collision
-            if (mitten.velocity.y > 0 && mitten.position.y + mitten.dimensions.height - mitten.velocity.y <= platform.position.y) {
-                mitten.velocity.y = 0;
-                mitten.midair = false;
-                mitten.position.y = platform.position.y - mitten.dimensions.height;
-            }
-            // Bottom collision
-            else if (mitten.velocity.y < 0 && mitten.position.y - mitten.velocity.y >= platform.position.y + platform.dimensions.height) {
-                mitten.velocity.y = 0;
-                mitten.position.y = platform.position.y + platform.dimensions.height;
-            }
-            // Left collision
-            if (mitten.velocity.x > 0 && mitten.position.x + mitten.dimensions.width - mitten.velocity.x <= platform.position.x) {
-                mitten.velocity.x = 0;
-                mitten.position.x = platform.position.x - mitten.dimensions.width - mitten.velocity.x;
-            }
-            // Right collision
-            else if (mitten.velocity.x < 0 && mitten.position.x - mitten.velocity.x >= platform.position.x + platform.dimensions.width) {
-                mitten.velocity.x = 0;
-                mitten.position.x = platform.position.x + platform.dimensions.width;
-            }
-        }
+function handleCollisions() {
+  platforms.forEach((platform) => {
+    if (
+      mitten.position.x < platform.position.x + platform.dimensions.width &&
+      mitten.position.x + mitten.dimensions.width > platform.position.x &&
+      mitten.position.y < platform.position.y + platform.dimensions.height &&
+      mitten.position.y + mitten.dimensions.height > platform.position.y
+    ) {
+      // Top collision
+      if (
+        mitten.velocity.y > 0 &&
+        mitten.position.y + mitten.dimensions.height - mitten.velocity.y <=
+          platform.position.y &&
+        mitten.position.x + mitten.dimensions.width - mitten.velocity.x >=
+          platform.position.x &&
+        mitten.position.x - mitten.velocity.x <=
+          platform.position.x + platform.dimensions.width
+      ) {
+        console.log('top');
+        mitten.velocity.y = 0;
+        mitten.position.y = platform.position.y - mitten.dimensions.height;
+        mitten.midair = false;
+        mitten.onPlatform = platform;
+        console.log(platform);
+      }
+      // Bottom collision
+      else if (
+        mitten.velocity.y < 0 &&
+        mitten.position.y - mitten.velocity.y >=
+          platform.position.y + platform.dimensions.height
+      ) {
+        console.log('bottom');
+        mitten.velocity.y = 0;
+        mitten.position.y = platform.position.y + platform.dimensions.height;
+      }
+      // Left collision
+      if (
+        mitten.velocity.x > 0 &&
+        mitten.position.x + mitten.dimensions.width - mitten.velocity.x <=
+          platform.position.x
+      ) {
+        mitten.velocity.x = 0;
+        mitten.position.x =
+          platform.position.x - mitten.dimensions.width - mitten.velocity.x;
+      }
+      // Right collision
+      else if (
+        mitten.velocity.x < 0 &&
+        mitten.position.x - mitten.velocity.x >=
+          platform.position.x + platform.dimensions.width
+      ) {
+        mitten.velocity.x = 0;
+        mitten.position.x = platform.position.x + platform.dimensions.width;
+      }
+    }
+  });
+
+  if (
+    mitten.onPlatform &&
+    !mitten.onPlatform.hasLeftObstacle &&
+    mitten.position.y ===
+      mitten.onPlatform.position.y - mitten.dimensions.height &&
+    mitten.position.x + mitten.dimensions.width < mitten.onPlatform.position.x
+  ) {
+    console.log('falling off');
+    mitten.midair = true;
+    mitten.onPlatform = undefined;
+  } else if (
+    mitten.onPlatform &&
+    !mitten.onPlatform.hasRightObstacle &&
+    mitten.position.y ===
+      mitten.onPlatform.position.y - mitten.dimensions.height &&
+    mitten.position.x + mitten.dimensions.width >
+      mitten.onPlatform.position.x + mitten.onPlatform.dimensions.width
+  ) {
+    console.log('falling off');
+    mitten.midair = true;
+    mitten.onPlatform = undefined;
+  }
+}
+
+function collectMice() {
+  mice.forEach((mouse, i) => {
+    if (
+      mitten.position.x < mouse.position.x + mouse.dimensions.width &&
+      mitten.position.x + mitten.dimensions.width > mouse.position.x &&
+      mitten.position.y < mouse.position.y + mouse.dimensions.height &&
+      mitten.position.y + mitten.dimensions.height > mouse.position.y
+    ) {
+      mice.splice(i, 1);
+      scoreBoard.score++;
+    }
+  });
+}
+
+function enemyAnimation() {
+  doggos.forEach((dog) => {
+    platforms.forEach((platform) => {
+      if (
+        dog.position.x + dog.dimensions.width + dog.velocity.x >=
+          platform.position.x &&
+        dog.position.x + dog.velocity.x <=
+          platform.position.x + platform.dimensions.width
+      ) {
+        dog.velocity.x *= -1;
+      }
     });
-
+  });
 }
 
-
-function collectMice () {
-    mice.forEach((mouse, i) => {
-        if (mitten.position.x < mouse.position.x + mouse.dimensions.width &&
-            mitten.position.x + mitten.dimensions.width > mouse.position.x &&
-            mitten.position.y < mouse.position.y + mouse.dimensions.height &&
-            mitten.position.y + mitten.dimensions.height > mouse.position.y) {
-            mice.splice(i, 1);
-            scoreBoard.score ++;
-        }
-    })
+function getHit() {
+  doggos.forEach((dog) => {
+    health.forEach((heart, i) => {
+      if (
+        mitten.position.x < dog.position.x + dog.dimensions.width &&
+        mitten.position.x + mitten.dimensions.width > dog.position.x &&
+        mitten.position.y < dog.position.y + dog.dimensions.height &&
+        mitten.position.y + mitten.dimensions.height > dog.position.y &&
+        !mitten.gotHit
+      ) {
+        mitten.gotHit = true;
+        health.splice(i, 1);
+        setTimeout(() => {
+          mitten.gotHit = false;
+        }, 2000);
+      }
+    });
+  });
 }
 
-function enemyAnimation () {
-    doggos.forEach (dog => {
-        platforms.forEach (platform => {
-            if(dog.position.x + dog.dimensions.width + dog.velocity.x >= platform.position.x &&
-                dog.position.x + dog.velocity.x <= platform.position.x + platform.dimensions.width) {
-                dog.velocity.x *= -1;
-            }
-        })
-    })
-}
-
-function getHit () {
-    doggos.forEach(dog=> {
-        health.forEach((heart,i) => {
-            if (mitten.position.x < dog.position.x + dog.dimensions.width &&
-                mitten.position.x + mitten.dimensions.width > dog.position.x &&
-                mitten.position.y < dog.position.y + dog.dimensions.height &&
-                mitten.position.y + mitten.dimensions.height > dog.position.y &&
-                !mitten.gotHit) {
-                mitten.gotHit = true;
-                health.splice(i, 1);
-                setTimeout(()=>{
-                    mitten.gotHit = false;
-                },2000);
-            }
-        })
-    })
-}
-
-export {handleCollisions, collectMice, enemyAnimation, getHit}
+export { handleCollisions, collectMice, enemyAnimation, getHit };
