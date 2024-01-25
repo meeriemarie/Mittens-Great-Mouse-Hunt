151e6bbc772ae90915bdb08df6bc0162d1d0c039
Jakob Bal <baljakob9@gmail.com>
2024-01-25 17:02:15 +0100

    Added attributes for obstacle detection

--- GameObjects/obstacle.js
+++ GameObjects/obstacle.js
@@ -1,20 +1,27 @@
-import {gameObject} from "./gameObject.js";
-import {ctx} from "../canvas.js";
+import { gameObject } from './gameObject.js';
+import { ctx } from '../canvas.js';
 
 class Obstacle extends gameObject {
-    constructor(width,height,x,y,imagePath) {
-        super(width,height,x,y);
-        this.imageObject = new Image();
-        this.imageObject.addEventListener("load", this.draw)
-        this.imageObject.src = imagePath;
-    }
+  constructor(width, height, x, y, imagePath) {
+    super(width, height, x, y);
+    this.imageObject = new Image();
+    this.imageObject.addEventListener('load', this.draw);
+    this.imageObject.src = imagePath;
+    this.hasLeftObstacle = false;
+    this.hasRightObstacle = false;
+  }
 
-    draw = (cx, cy) => {
-        ctx.save();
-        ctx.drawImage(this.imageObject,this.position.x + cx,this.position.y + cy,this.dimensions.width,this.dimensions.height);
-        ctx.restore();
-    }
-
+  draw = (cx, cy) => {
+    ctx.save();
+    ctx.drawImage(
+      this.imageObject,
+      this.position.x + cx,
+      this.position.y + cy,
+      this.dimensions.width,
+      this.dimensions.height
+    );
+    ctx.restore();
+  };
 }
 
-export {Obstacle}
+export { Obstacle };
